dc362dcd31a3dd4854b7a0412d70ae869d451f11
Adrian Arceredillo <arceredillo.adrian@uni.eus>
2023-01-16 13:27:02 +0100

    .

--- Datu_Atzipena/restapi_0/restapi/src/main/java/com/dambi/restapi/MainController.java
+++ Datu_Atzipena/restapi_0/restapi/src/main/java/com/dambi/restapi/MainController.java
@@ -105,5 +105,29 @@ public class MainController {
         return langileaRepository.findById(langileaEmail);
     }
     
+    @GetMapping("/zenbatu")
+	public String zenbatu(@RequestParam(value = "norarte", defaultValue = "1000") String norarte) {
+
+		String strEmaitza = "";
+		for (int i = 1; i <= Integer.parseInt(norarte); i++) {
+			// strEmaitza += "<body style=\"background-color:grey;\">";
+			// Tema_Spring\lehen\lehen_restapia\imagenes
+			strEmaitza += "<body style=\"background-image:url('https://www.banderasphonline.com/wp-content/uploads/2020/03/bandera-espa%C3%B1a-con-escudo-para-exterior-interior-1200x800.png');background-repeat: no-repeat;background-attachment: fixed;background-size: cover; width:100%; height:auto\"></body>";
+			
+			if ((i % 2) == 0) {
+				// strEmaitza += (i + " <br/><label style=\"color:yellow; font-size: 60px\">O
+				// quizás no</label><br/>");
+				// strEmaitza += (i + " <label style=\"color:yellow;\">O quizás no</label>"); ༼
+				// つ ◕_◕ ༽つ
+				strEmaitza += (i + " <label style=\"color:yellow;\">ESPAÑITA gana el Mundial!!!</label>");
+			} else {
+				// strEmaitza += (i + " <br/><label style=\"color:red; font-size: 100px\">ESPAÑA
+				// GANA EL MUNDIAL!!!</label><br/>");
+				strEmaitza += (i + " <label style=\"color:black;font-size: 40px\">༼ つ ◕_◕ ༽つ</label>");
+			}
+		}
+		return strEmaitza;
+	}
+
 
 }

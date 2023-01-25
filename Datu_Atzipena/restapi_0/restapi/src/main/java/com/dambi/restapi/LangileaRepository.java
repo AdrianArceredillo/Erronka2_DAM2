550bb2947662538d55b26e86379daf643ed0b2bf
Adrian Arceredillo <arceredillo.adrian@uni.eus>
2023-01-25 10:17:28 +0100

    .

--- Datu_Atzipena/restapi_0/restapi/src/main/java/com/dambi/restapi/LangileaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dambi.restapi;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dambi.restapi.pojoak.Langilea;
-
-public interface LangileaRepository extends CrudRepository<Langilea, String> {
-    
-}
